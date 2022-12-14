a3816d1a95f743063fad9141f6e5968dc0f8c729
erenkaraboga <darkbull1073@gmail.com>
2022-12-14 10:30:22 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:stm_mobile_new/views/BluetoothOfScreen.dart';
+import 'package:stm_mobile_new/views/FindDeviceScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder<BluetoothState>(
+          stream: FlutterBlue.instance.state,
+          initialData: BluetoothState.unknown,
+          builder: (c, snapshot) {
+            final state = snapshot.data;
+            if (state == BluetoothState.on) {
+              return FindDeviceScreen();
+            }
+            return BluetoothOffScreen(state: state);
+          }),
+    );
+  }
+}
+
---
+++ lib/views/FindDeviceScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class FindDeviceScreen extends StatefulWidget {
+  const FindDeviceScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FindDeviceScreen> createState() => _FindDeviceScreenState();
+}
+
+class _FindDeviceScreenState extends State<FindDeviceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
